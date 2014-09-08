b3232babccd4d63d973438fa71605bda675fdaf2
fanfank <reetsee@qq.com>
2014-09-08 23:30:21 +0800

    Fixed a bug

--- phpfetcher.php
+++ phpfetcher.php
@@ -1,8 +1,8 @@
 <?php
 define('PHPFETCHER_PATH', dirname(__FILE__));
 function __autoload($strClassName) {
-    if (substr($strClassName, 0, count('Phpfetcher_')) === 'Phpfetcher_') {
-        require_once PHPFETCHER . str_replace('_', '/', $strClassName) . '.php';
+    if (substr($strClassName, 0, strlen('Phpfetcher_')) === 'Phpfetcher_') {
+        require_once PHPFETCHER_PATH . str_replace('_', '/', $strClassName) . '.php';
     }
 }
 spl_autoload_register('__autoload');
