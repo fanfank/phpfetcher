36b119422f61a87c201c4f1dcee5a43907b9ae1b
fanfank <reetsee@qq.com>
2014-09-21 11:10:00 +0800

    Fixed bug in phpfetcher autoload

--- phpfetcher.php
+++ phpfetcher.php
@@ -1,9 +1,9 @@
 <?php
 define('PHPFETCHER_PATH', dirname(__FILE__));
-function __autoload($strClassName) {
+function __phpfetcher_autoload($strClassName) {
     if (substr($strClassName, 0, strlen('Phpfetcher_')) === 'Phpfetcher_') {
-        require_once PHPFETCHER_PATH . str_replace('_', '/', $strClassName) . '.php';
+        require_once PHPFETCHER_PATH . '/' . str_replace('_', '/', $strClassName) . '.php';
     }
 }
-spl_autoload_register('__autoload');
+spl_autoload_register('__phpfetcher_autoload');
 ?>
