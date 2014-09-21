c52fc2ab115ec74531e8e2ecdea387d62005b975
fanfank <reetsee@qq.com>
2014-09-21 11:30:16 +0800

    Finished extra info

--- Phpfetcher/Crawler/Abstract.php
+++ Phpfetcher/Crawler/Abstract.php
@@ -6,8 +6,9 @@
  *       Crawler objects' abstract class
  */
 abstract class Phpfetcher_Crawler_Abstract {
-    protected $arrPostFetchHooks = array();
-    protected $arrPreFetchHooks  = array();
+    protected $_arrPostFetchHooks = array();
+    protected $_arrPreFetchHooks  = array();
+    protected $_arrExtraInfo = array();
 
     //设置爬虫的爬取规则
     abstract function setFetchJobs($arrInput);
@@ -23,6 +24,25 @@ abstract class Phpfetcher_Crawler_Abstract {
     //对于每次爬取到的页面，进行的操作，这个方法需要使用者自己实现
     abstract function handlePage($objPage);
 
+    public function getExtraInfo($arrInput = array()) {
+        $arrOutput = array();
+        foreach ($arrInput as $field) {
+            $arrOutput[$field] = $this->_arrExtraInfo[$field];
+        }
+        $return $arrOutput;
+    }
+
+    public function setExtraInfo($arrInput = array()) {
+        if (!is_array($arrInput) || empty($arrInput) {
+            return FALSE;
+        }
+
+        foreach ($arrInput as $key => $value) {
+            $this->_arrExtraInfo[$key] = $value;
+        }
+
+        return TRUE;
+    }
     /*
     //修改一条已有的规则
     public function setFetchJobByName() {
