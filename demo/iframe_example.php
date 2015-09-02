5cd86592e219acdbc39f25d06272d5dfa8669bf4
fanfank <reetsee@qq.com>
2015-09-02 22:28:13 +0800

    add iframe_example.php

--- demo/iframe_example.php
+++ demo/iframe_example.php
@@ -1,33 +1,40 @@
 <?php
-//下面两行使得这个项目被下载下来后本文件能直接运行
 $demo_include_path = dirname(__FILE__) . '/../';
 set_include_path(get_include_path() . PATH_SEPARATOR . $demo_include_path);
 
 require_once('phpfetcher.php');
 class mycrawler extends Phpfetcher_Crawler_Default {
     public function handlePage($page) {
-        echo "Current Url: [" . $page->getUrl() . "]\n";
+        //echo "Current Url: [" . $page->getUrl() . "]\n";
 
-        //$arrScripts = $page->sel('//iframe');
-        //echo count($arrScripts) . "\n";
+        ////$arrScripts = $page->sel('//iframe');
+        ////echo count($arrScripts) . "\n";
 
-        //选取所有包含src属性的iframe元素
-        $arrIframes = $page->sel('//iframe'); 
-        for ($i = 0; $i < count($arrIframes); ++$i) {
-            $strSrc = $arrIframe[$i]->src; //some problem
-            echo "add iframe src=[" . $strSrc . "] to next depth\n";
-            $this->addAdditionalUrls($strSrc);
-        }
+        ////选取所有包含src属性的iframe元素
+        //$arrIframes = $page->sel('//iframe[@src]'); 
+        //for ($i = 0; $i < count($arrIframes); ++$i) {
+        //    //echo print_r(get_class($arrIframes[$i]), true) . "\n";
+        //    $strSrc = $arrIframes[$i]->src; //some problem
+        //    echo "add iframe src=[" . $strSrc . "] to next depth\n";
+        //    //$this->addAdditionalUrls($strSrc);
+        //    break;
+        //}
+        //echo "Finish Url: [" . $page->getUrl() . "]\n";
     }
-}
+
+};
 
 $crawler = new mycrawler();
 $arrJobs = array(
-    'qqnews' => array( 
+    '163' => array( 
         'start_page' => 'http://news.163.com',
         'link_rules' => array(),
         'max_depth' => 2, 
     ) ,   
 );
+print_r(get_class_methods(mycrawler));
+die(0);
+
+$crawler->addAdditionalUrls('http://news.qq.com');
 
 $crawler->setFetchJobs($arrJobs)->run();
